39591d8e952024f04fe3b01febb37af1878cac08
Juan Carlos <juancarlospaco@gmail.com>
2013-10-31 08:49:55 -0300

    add django smarturls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,18 +1,22 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include    # , url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+# amitu.com/smarturls
+from smarturls import surl
+
+
 urlpatterns = patterns('',
-    url(r'^$', include('polls.urls', namespace="polls")),
-    url(r'^login/', include('principal.urls', namespace="principal")),
-    url(r'^agenda/', include('agenda.urls', namespace="agenda")),
-    url(r'^polls/', include('polls.urls', namespace="polls")),
-    url(r'^developers/', include('developers.urls', namespace="developers")),
-    url(r'^recover_password/', include('recover_password.urls', namespace="recover_password")),
-    url(r'^admin/', include(admin.site.urls)),
+    surl('/', include('polls.urls', namespace="polls")),
+    surl('/login/', include('principal.urls', namespace="principal")),
+    surl('/agenda/', include('agenda.urls', namespace="agenda")),
+    surl('/polls/', include('polls.urls', namespace="polls")),
+    surl('/developers/', include('developers.urls', namespace="developers")),
+    surl('/recover_password/', include('recover_password.urls', namespace="recover_password")),
+    surl('/admin/', include(admin.site.urls)),
 )
 
 urlpatterns += staticfiles_urlpatterns()
