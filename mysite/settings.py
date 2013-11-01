d7c939b78a3972fded564cd1464a3632c9515e7b
Juan Carlos <juancarlospaco@gmail.com>
2013-11-01 00:14:27 -0300

    simplify agenda

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,12 +113,11 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'userswitch.middleware.UserSwitchMiddleware',
 )
 
 ROOT_URLCONF = 'mysite.urls'
